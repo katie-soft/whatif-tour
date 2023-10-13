a84b44638b77f51865b536350f492588978c4865
katie-soft <analyst.meo@gmail.com>
2023-10-13 17:47:30 +0300

    feat: add mobile version of sliders

--- script/script.js
+++ script/script.js
@@ -49,17 +49,29 @@ document.addEventListener('click', (event) => {
 const feedbackSlider = new Swiper('.feedback-swiper', {
   loop: true,
 
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+
+  breakpoints: {
+    750: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 30
+    },
+    900: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 24
+    },
+  },
+
   navigation: {
     nextEl: '.feedback__controls .swiper-button-next',
     prevEl: '.feedback__controls .swiper-button-prev',
   },
-
-  slidesPerView: 3,
-  slidesPerGroup: 3,
-  spaceBetween: 24
 });
 
-/* Slider for videp */
+/* Slider for video */
 
 const videoSlider = new Swiper('.video-swiper', {
     slidesPerView: 1,
@@ -68,4 +80,36 @@ const videoSlider = new Swiper('.video-swiper', {
         el: '.video-swiper__pagination.swiper-pagination',
         clickable: true
     },
+});
+
+/* Slider for cards */
+
+const cardSlider = new Swiper('.cards-swiper', {
+  loop: true,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  autoplay: {
+   delay: 2000,
+ },
+
+  breakpoints: {
+    750: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 20,
+      loop: true,
+    },
+        900: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 24,
+      loop: false,
+      autoplay: false
+    },
+  },
+    
+  navigation: {
+    nextEl: '.cards__controls .swiper-button-next',
+    prevEl: '.cards__controls .swiper-button-prev',
+  },
 });
